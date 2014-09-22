e6cc13c61ae87744e9a0124729523885fcd4d689
Jess Chadwick <jesschadwick@gmail.com>
2014-09-22 01:42:53 -0400

    Implementing QuizService

---
+++ app/services/QuizService.spec.ts
@@ -0,0 +1,108 @@
+/// <reference path="../model.ts" />
+/// <reference path="../../testing/StubData.ts" />
+/// <reference path="../../typings/angularjs/angular.d.ts" />
+/// <reference path="../../typings/angularjs/angular-resource.d.ts" />
+/// <reference path="../../typings/angularjs/angular-mocks.d.ts" />
+/// <reference path="../../typings/jasmine/jasmine.d.ts" />
+/// <reference path="../../typings/jasmine/jasmine-matchers.d.ts" />
+/// <reference path="../../testing/matchers.d.ts" />
+
+var $httpBackend: ng.IHttpBackendService;
+
+module Quizzical {
+    'use strict';
+
+    describe('QuizService', () => {
+
+        var service;
+
+        beforeEach(inject((QuizService: IQuizService) => {
+            service = QuizService;
+        }));
+
+
+        it('should get all quizzes', (done) => {
+            var quiz: Quiz = { id: 1, name: 'Test Quiz', questions: [] };
+
+            $httpBackend.expectGET(getApiUrl())
+                        .respond([quiz]);
+
+            service.getAll().then(resp => {
+                expect(resp[0].id).toBe(quiz.id);
+                expect(resp[0].name).toBe(quiz.name);
+                done();
+            });
+
+            $httpBackend.flush();
+        });
+
+        it('should get quiz by id', (done) => {
+            var quiz: Quiz = { id: 1, name: 'Test Quiz', questions: [] };
+
+            $httpBackend.expectGET(getApiUrl(quiz.id))
+                        .respond(quiz);
+
+            service.getById(quiz.id).then(resp => {
+                expect(resp.id).toBe(quiz.id);
+                expect(resp.name).toBe(quiz.name);
+                done();
+            });
+
+            $httpBackend.flush();
+        });
+
+        it('should create new quiz', (done) => {
+            var quiz: Quiz = { id: null, name: 'Test Quiz', questions: [] };
+
+            $httpBackend.expectPOST(getApiUrl())
+                        .respond(quiz);
+
+            service.save(quiz)
+                .then(resp => {
+                    expect(resp.id).toBe(quiz.id);
+                    expect(resp.name).toBe(quiz.name);
+                    done();
+                });
+
+            $httpBackend.flush();
+        });
+
+        it('should delete existing quiz by id', (done) => {
+            var quizId = 123;
+
+            $httpBackend.expectDELETE(getApiUrl(quizId))
+                        .respond(200);
+
+            service.delete(quizId)
+                   .then(done);
+
+            $httpBackend.flush();
+        });
+
+        it('should update existing quiz', (done) => {
+            var updated: Quiz = { id: 123, name: 'Updated Test Quiz', questions: [] };
+
+            $httpBackend.expectPUT(getApiUrl(updated.id))
+                .respond(updated);
+
+            service.save(updated).then(resp => {
+                expect(resp.id).toBe(updated.id);
+                expect(resp.name).toBe(updated.name);
+                done();
+            });
+
+            $httpBackend.flush();
+        });
+
+
+        function getApiUrl(quizId?: number, action?: string) {
+            var parts : any[] = ['/api/quizzes'];
+
+            if (quizId) parts.push(quizId);
+            if (action) parts.push(action);
+
+            return parts.join('/');
+        }
+
+    });
+}
--- app/services/QuizService.ts
+++ app/services/QuizService.ts
@@ -1,18 +1,76 @@
 /// <reference path="../model.ts" />
+/// <reference path="../../typings/angularjs/angular.d.ts" />
+/// <reference path="../../typings/angularjs/angular-resource.d.ts" />
 
 module Quizzical {
     'use strict';
 
     export interface IQuizService {
-        list(): Quiz[];
-        save(quiz: Quiz): Quiz;
+        delete(quiz: Quiz): ng.IPromise<void>;
+        delete(quizId: number): ng.IPromise<void>;
+        getAll(): ng.IPromise<Quiz[]>;
+        getById(quizId: number): ng.IPromise<Quiz>;
+        save(quiz: Quiz): ng.IPromise<Quiz>;
     }
 
-    class QuizSessionService {
+    interface IQuiz extends Quiz, ng.resource.IResource<IQuiz> {
+    }
+
+    interface IQuizDataSource extends ng.resource.IResourceClass<IQuiz> {
+        update(quiz: Quiz): ng.IPromise<Quiz>;
+    }
+
+    class QuizService implements IQuizService {
+
+        static $inject = ['QuizDataSource'];
+
+        constructor(private QuizDataSource: IQuizDataSource) {
+            this.CreateQuizCommand = (q?: any) => {
+              return new QuizDataSource(q);
+            };
+        }
+
+        private CreateQuizCommand(q?: any): IQuiz {
+            throw 'NotImplemented';
+        }
+
+        'delete'(quiz:Quizzical.Quiz): ng.IPromise<void>;
+        'delete'(quizId:number): ng.IPromise<void>;
+        'delete'(parm: any): ng.IPromise<void> {
+            var quizId : number =
+                angular.isNumber(parm)
+                    ? parm
+                    : (parm && parm.id ? parm.id : null);
+
+            return (<any>this.QuizDataSource.delete({ quizId: quizId })).$promise;
+        }
+
+        getAll():ng.IPromise<Quizzical.Quiz[]> {
+            return (<any>this.QuizDataSource.query()).$promise;
+        }
+
+        getById(quizId:number):ng.IPromise<Quizzical.Quiz> {
+            return this.CreateQuizCommand({ quizId: quizId }).$get();
+        }
+
+        save(quiz:Quizzical.Quiz):ng.IPromise<Quizzical.Quiz> {
+            if(!quiz.id) {
+                return this.CreateQuizCommand(quiz).$save();
+            } else {
+                return (<any>this.QuizDataSource.update(quiz)).$promise;
+            }
+        }
 
     }
 
     angular.module('Quizzical.Services')
-        .service('QuizSessionService', QuizSessionService);
+        .factory('QuizDataSource', ['$resource', ($resource: ng.resource.IResourceService) => {
+            return $resource<QuizSession>('/api/quizzes/:quizId', { 'quizId': '@quizId'}, {
+                'update': { method: 'PUT', params: { 'quizId': '@id' } }
+            });
+        }]);
+
+    angular.module('Quizzical.Services')
+        .service('QuizService', QuizService);
 
 }
