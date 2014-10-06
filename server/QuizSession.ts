e80fc25f89533998c3d82628d5da1e0b317fc21e
Jess Chadwick <jesschadwick@gmail.com>
2014-10-06 00:22:18 -0400

    Splitting QuizSession and Connected Users logic

--- server/QuizSession.ts
+++ server/QuizSession.ts
@@ -1,51 +1,12 @@
 ﻿declare var exports: any;
 
-var io = require('socket.io');
-
-var connectedUsers = [];
+var io = require('socket.io')();
 
 class QuizSession {
-    private socket;
-
-    constructor(server) {
-        this.socket = io(server);
-    }
 
     changeQuestionId(questionId: number) {
         console.log('broadcasting currentQuestionId: ' + questionId);
-        this.socket.emit('question.changed', { questionId: questionId });
-    }
-
-    connectUser(userId: string) {
-        var user = this.getConnectedUser(userId);
-
-        if (!user) {
-            var username = userId;  // TODO: Fetch username
-            user = <Quizzical.User>{ id: userId, name: username };
-            connectedUsers.push(user);
-        }
-
-        return user;
-    }
-
-    disconnectUser(userId: string) {
-        var user = this.getConnectedUser(userId),
-            index = connectedUsers.indexOf(user);
-
-        if (index) {
-            connectedUsers.splice(index, 1);
-        }
-
-        return user;
-    }
-
-    getConnectedUser(userId: string) {
-        var users = connectedUsers.filter(u => u.id == userId);
-        return users.length ? users[0] : null;
-    }
-
-    getConnectedUsers() {
-        return connectedUsers;
+        io.emit('question.changed', { questionId: questionId });
     }
 
 }
