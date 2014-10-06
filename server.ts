93900512fe01e8d26984051c3167e2396e7aa72c
Jess Chadwick <jesschadwick@gmail.com>
2014-10-05 22:15:16 -0400

    Adding express-livereload

--- server.ts
+++ server.ts
@@ -3,7 +3,8 @@ declare var require, __dirname, process;
 var express = require('express'),
     http = require('http'),
     api = require('./server/api'),
-    realtime = require('./server/realtime');
+    realtime = require('./server/realtime'),
+    livereload = require('express-livereload');
 
 var app = express(),
     server = http.Server(app);
@@ -14,6 +15,8 @@ app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 api(app, server);
 realtime(server);
+
+livereload(app, {});
 
 var port = process.env.PORT || 3000;
 server.listen(port, () => {
